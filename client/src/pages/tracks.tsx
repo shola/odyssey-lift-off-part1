3100af28ed03c8380949d5642cc18fcaf444491a
shola <michael.o.situ@gmail.com>
2024-12-15 22:50:54 -0800

    feat: create tracksForHome query chore: move author field to end of Track definition so nested objects appear at end for better readability

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__/";
 
+// Define queries with components that use them instead of having
+// a massive folder somewhere full of all queries
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -5,29 +5,29 @@ import { gql } from "graphql-tag";
 // Object types that the graphql API can return. These model
 // the business domain
 const typeDefs = gql`
-    """
-    Each query field is an entrypoint into the rest of the schema.
-    It defines what data clients can query in the schema, equivalent
-    to REST endpoints.
-    """
-    type Query {
-        "Query to get tracks array for the homepage grid"
-        tracksForHome: [Track!]!
-    }
+  """
+  Each query field is an entrypoint into the rest of the schema.
+  It defines what data clients can query in the schema, equivalent
+  to REST endpoints.
+  """
+  type Query {
+      "Query to get tracks array for the homepage grid"
+      tracksForHome: [Track!]!
+  }
 
-"A track is a group of modules that teaches about a specific topic"
+  "A track is a group of modules that teaches about a specific topic"
   type Track {
     id: ID!
     "the track's title"
     title: String!
     "the track's main illustration to display in track card or track page detail"
     thumbnail: String
-    "the track's main author"
-    author: Author!
     "the track's approximate length to complete, in minutes"
     length: Int
     "the number of modules this track contains"
-    modulesCount: Int
+    modulesCount: Int    
+    "the track's main author"
+    author: Author!
   }
 
 "Author of a complete Track"
