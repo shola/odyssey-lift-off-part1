c0780c7237b429d77dc8c4284c17a96f9d072561
shola <michael.o.situ@gmail.com>
2024-12-15 18:25:29 -0800

    feat: create apollo server feat: instead of creating the server with typeDefs, use mocked data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,43 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { typeDefs } from "./schema";
+
+const mocks = {
+    Query: () => ({
+        tracksForHome: [...new Array(6)]
+    }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    thumbnail: () =>
+      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+
+    author: () => ({
+      name: "Grumpy Cat",
+      photo:
+        "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+    }),
+    length: () => 1210,
+    moduleCount: () => 6,
+  }),
+};
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({
+        typeDefs,
+      }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀 Server is running!
+    📭 Query at ${url}
+    `);
+}
+
+startApolloServer();
